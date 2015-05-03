23c6baf6937ca84a7dddeb9f62f2cc74380692d3
Konstantin Gulyy <const.gulyy@gmail.com>
2015-05-03 19:07:17 +0300

    Initial commit

---
+++ TASKS/src/main/java/ru/bmstu/iu7/rsoi/gulyy/corsework/tasks/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package ru.bmstu.iu7.rsoi.gulyy.corsework.tasks;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Konstantin Gulyy
+ */
+
+@ApplicationPath("/")
+public class ApplicationConfig extends Application {
+
+}
